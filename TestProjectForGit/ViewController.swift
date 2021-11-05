cf5f1531545f748c618b239d1459d1a0414e4aa7
RAMENSKII <RA>
2021-11-05 11:45:59 +0700

    Initial Commit

---
+++ TestProjectForGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProjectForGit
+//
+//  Created by Kirill on 05.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
